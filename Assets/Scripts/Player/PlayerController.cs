9d88459ea11fab5a7e388f13e0ee21110431eaac
liaoszong <jinqiong_chen@163.com>
2023-11-09 13:26:51 +0800

    11.9

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerinputControl inputControl;
+    public Rigidbody2D rb;
+    public Vector2 inputDirection;
+    public float speed;
+
+    private void Awake()
+    {
+        inputControl = new PlayerinputControl();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;
+
+        if (inputDirection.x > 0)
+            faceDir = 1;
+        if (inputDirection.x < 0)
+            faceDir = -1;
+
+        //人物翻转
+        transform.localScale = new Vector3(faceDir,1,1);
+    }
+}
