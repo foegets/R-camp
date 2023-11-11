9fec3c4729c4ac2a901e9579368931e57cce7d62
Berluga <149092042+Berluga@users.noreply.github.com>
2023-11-11 09:06:36 +0800

    一个可以移动和跳跃的游戏

---
+++ Assets/scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    public Vector2 inputDirection;
+    [Header("基本参数")]
+    public float speed;
+    public float jumpForce;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputControl = new PlayerInputControl();
+
+        inputControl.GamePlayer.Jump.started += Jump;
+    }
+
+    
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.GamePlayer.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(); 
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime,rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+            faceDir = 1;
+        if (inputDirection.x < 0)
+            faceDir = -1;
+        //人物翻转 
+        transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        //Debug.Log("Jump");
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
