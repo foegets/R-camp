c131a79be7330ba8a1095da6c33eca7dae56858e
falling-fifteen <3054965007@qq.com>
2023-11-12 13:42:39 +0800

    add jumpAnimation and virtual camera

--- Assets/script/player/PlayerAnimation.cs
+++ Assets/script/player/PlayerAnimation.cs
@@ -7,11 +7,13 @@ public class PlayerAnimation : MonoBehaviour
     private Animator anim;//创建变量用于获得通过速度切换动画的animator
     private Rigidbody2D rb;//创建变量用于获得速度来传进velocityX
     private PlayerController playerController;
+    private PhysicsCheck physicsCheck;
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         playerController = GetComponent<PlayerController>();
+        physicsCheck = GetComponent<PhysicsCheck>();
     }
     private void Update()
     {
@@ -21,7 +23,9 @@ public class PlayerAnimation : MonoBehaviour
     {
         //传进速度后取绝对值避免向左走速度为负而无法转换动作的问题
         anim.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocityY", rb.velocity.y);
         anim.SetBool("isDead", playerController.isDead);
+        anim.SetBool("isGround", physicsCheck.isGround);
     }
 
     //实现受伤后播放闪烁动画
