586b3804f355839db6df927ed080c67da9f3a548
Umaruuuuuu <149139525+Umaruuuuuu@users.noreply.github.com>
2023-11-19 00:04:42 +0800

    homework-Week2
    不完整版2.0

---
+++ Assets/Scripts/Collection/collection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collection : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
