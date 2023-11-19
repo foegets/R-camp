d106483559ffc968c74479a845a2d1f7b37d6141
Umaruuuuuu <149139525+Umaruuuuuu@users.noreply.github.com>
2023-11-19 21:47:29 +0800

    homework-Week2
    5.0

--- Assets/Scripts/Collection/collection.cs
+++ Assets/Scripts/Collection/collection.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class collection : MonoBehaviour
-{
+{   
+    public scoreCounter t;
+    public Text score;
+
     private void OnTriggerStay2D(Collider2D other)
     {
+        //计分
+        t.amount ++;
+        score.text = t.amount.ToString();
+
+        //物体消失
         Destroy(this.gameObject);
     }
 }
+
