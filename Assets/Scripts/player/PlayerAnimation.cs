d0c20a6ed18275da1bbb606b5589902313a40c4c
kasaly39 <1829895472@qq.com>
2023-11-19 12:23:20 +0800

    2

---
+++ Assets/Scripts/player/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        SetAnimation();
+    }
+    public void SetAnimation()
+    {
+        anim.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
+    }
+}
