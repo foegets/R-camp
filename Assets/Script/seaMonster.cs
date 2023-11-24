62ccb9622cde019e323ef8d6e2ba15a7dea45c6f
KnowOnlyalittle <114860853+foegets@users.noreply.github.com>
2023-11-24 23:09:23 +0800

    AI
    巡逻ai

--- Assets/Script/seaMonster.cs
+++ Assets/Script/seaMonster.cs
@@ -4,16 +4,97 @@ using UnityEngine;
 
 public class seaMonster : Enemy
 {
-    // Start is called before the first frame update
+    public float startWaitTime;
+    public float waitTime;
+    public float speed;
+
+    public bool isFind;
+
+    public Transform movePos;
+    public Transform leftPos;
+    public Transform rightPos;
+    public Animator ani;
+    private Rigidbody2D rig;
+
     void Start()
     {
+        Debug.Log("1");
         base.Start();
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+        movePos.position= new Vector2(Random.Range(leftPos.position.x, rightPos.position.x), transform.position.y);
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    { 
         base.Update();
+        pursuit();
+        Run();
     }
 
+   void pursuit()
+   {
+        transform.position = Vector2.MoveTowards(transform.position, movePos.position, speed * Time.deltaTime);
+        Filp();
+        if(!isFind)
+        {  
+            Debug.Log("pursuit");
+            if (Vector2.Distance(transform.position, movePos.position) < 0.1f)
+            {
+                if(waitTime<0.1f)
+                {
+                    movePos.position = new Vector2(Random.Range(leftPos.position.x, rightPos.position.x), transform.position.y);
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+         
+        
+   }
+
+
+    void Run()
+    {
+        if(transform.position!=movePos.position)
+        {
+            ani.SetBool("isRun", true);
+        }
+        else
+        {
+            ani.SetBool("isRun", false);
+        }
+    }
+
+    void Filp()
+    {
+         if (transform.position.x<movePos.position.x)
+         {
+             transform.localRotation = Quaternion.Euler(0, 0, 0);
+         }
+         else
+         {
+             transform.localRotation = Quaternion.Euler(0, 180, 0);
+         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("onttigger");
+            isFind = true;
+            movePos.position = new Vector2(other.transform.position.x,transform.position.y);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFind = false;
+        }
+    }
 }
