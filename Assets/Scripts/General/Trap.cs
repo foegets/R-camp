10cda35b97b39f86c4ce94f9dd1df600edbe2eaf
Nikko-Cat <774161722@qq.com>
2023-11-26 17:20:39 +0800

    Week3-homework

--- Assets/Scripts/General/Trap.cs
+++ Assets/Scripts/General/Trap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Trap : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Trap : MonoBehaviour
         if(other.gameObject.tag == "Spike")
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
     }
 }
--- Assets/Scripts/UI/CollectionUI.cs
+++ Assets/Scripts/UI/CollectionUI.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CollectionUI : MonoBehaviour
 {
     public int startCollectionQuantity;
+    public int TotalCollectionQuantity;
     public TextMeshProUGUI collectionQuantity;//TMP_Text
 
     public static int CurrentCollectionQuantity;
@@ -21,5 +23,14 @@ public class CollectionUI : MonoBehaviour
     void Update()
     {
         collectionQuantity.text = CurrentCollectionQuantity.ToString();
+        check();
+    }
+
+    void check()
+    {
+        if(CurrentCollectionQuantity == TotalCollectionQuantity)
+        {
+            SceneManager.LoadScene(3);
+        }
     }
 }
