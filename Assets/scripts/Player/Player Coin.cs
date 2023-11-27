5abaca0a3f34abc809a3f97b7b7da9ee4c22e207
Berluga <149092042+Berluga@users.noreply.github.com>
2023-11-27 23:39:59 +0800

    补上捡金币的功能，我终于做出来辣

--- Assets/scripts/Player/Player Coin.cs
+++ Assets/scripts/Player/Player Coin.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCoin : MonoBehaviour
 {
+    public TMP_Text UI;
+    public int Score = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +19,14 @@ public class PlayerCoin : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)//检测is trigger碰撞体进入
     {
-        if (other.Equals("coin") ){
-            
+        if (other.tag=="Coin" )   //检测进入的是不是coin//还可以用CompareTag("Coin")
+        {   
+            Score++;//分数+1
+            UI.text = "Score:" + Score;//改变ui
+            Destroy(other.gameObject);//coin消失
+
         }
     }
 }
