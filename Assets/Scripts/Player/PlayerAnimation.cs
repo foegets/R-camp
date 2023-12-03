6833350754d96e7468f9e085640f0a425ea8200e
P1aneZ <148950245+P1aneZ@users.noreply.github.com>
2023-12-03 20:23:47 +0800

    一些注释的修改

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -14,8 +14,8 @@ public class PlayerAnimation : MonoBehaviour
 
 
     private void Awake(){
-        anim=GetComponent<Animator>();
-        rb= GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();//GetComponent是获取当前游戏对象组件的方法，可以通过直接调用它来访问游戏对象的组件和进行参数调整
+        rb = GetComponent<Rigidbody2D>();
         physicsCheck=GetComponent<PhysicsCheck>();
         playerController=GetComponent<PlayerController>();
     }
@@ -25,7 +25,7 @@ public class PlayerAnimation : MonoBehaviour
     }
 
     public void SetAnimation(){
-        //把PlayerController中的变量和animator中作判断条件的数值关联起来（？）
+        //把PlayerController中的变量和animator中作判断条件的数值关联起来,更准确的说是赋值！
         anim.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
         anim.SetFloat("velocityY",rb.velocity.y);
         anim.SetBool("isGround",physicsCheck.isGround);
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,15 +8,23 @@ using UnityEngine.Events;
 public class PlayerController : MonoBehaviour
 {/*笔记：private类型的变量只能在当前类（class）中使用，而public则可以在其他class中被调用，且可以在Inspect窗口中看到*/
     public UnityEvent OnDie;
+
     public PlayerInputControl inputControl;
-    private Rigidbody2D rb;//rb是
-    private PhysicsCheck physicsCheck;
+
+    private Rigidbody2D rb;//rb是刚体的类型
+
+    private PhysicsCheck physicsCheck;//private类型的变量只能在这个类中被调用
+
     private Character character;
+
     [Header("输入的移动方向")]
     public Vector2 inputDirection;//用来存储Vector2类型的变量,是用于储存输入的方向
+
     [Header("基本参数")]
     public float speed;//速度的值
+
     public float jumpForce;//跳跃的力
+
     public float exitTime = 300;
 
     public bool isHurt;//判断是否受伤
@@ -26,7 +34,7 @@ public class PlayerController : MonoBehaviour
     public float hurtForce;//受伤时受到的冲击力
 
     private void Awake(){
-        rb=GetComponent<Rigidbody2D>();
+        rb=GetComponent<Rigidbody2D>();//变量的赋值
         physicsCheck=GetComponent<PhysicsCheck>();
         character=GetComponent<Character>();
 
@@ -70,9 +78,6 @@ public class PlayerController : MonoBehaviour
         if(transform.position.y < -11.5) {
             rb.velocity = new Vector2(0, 0);
             OnDie?.Invoke();/*触发死亡的函数*/
-
-
-
         }
     }
 
@@ -82,12 +87,13 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate(){//跟物理有关的都放在FixedUpdate执行
-        if (!isHurt)
+        if (!isHurt)//如果没有处于受伤状态
         Move();//人物移动
     }
 
     public void Move()
-    {//人物移动
+    {
+        //人物移动
         rb.velocity=new Vector2(inputDirection.x*speed*Time.deltaTime,rb.velocity.y);
 
         //实现人物的翻转（转向）
